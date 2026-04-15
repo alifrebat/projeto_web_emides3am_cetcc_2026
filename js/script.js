a73cd426ed2d0e99aeb90e92aa1500fc2991a1a5
dafukf <dl.aquino91@outlook.com>
2026-04-15 09:10:52 -0300

    Criação da função responsável por renderizar Produtos

--- js/script.js
+++ js/script.js
@@ -24,3 +24,59 @@ const removerProduto = (idProduto) => {
     )
     renderizarProdutos()
 }
+
+const renderizarProdutos = () => {
+    const containerVitrine = document.getElementById('lista-produtos')
+    containerVitrine.innerHTML = ''
+
+    produtosCadastrados.forEach((produto) => {
+        const { id, nome, caracteristicas, valorUnitario, unidade, tipo } =
+            produto
+
+        const cardProduto = document.createElement('div')
+        cardProduto.classList.add('card-produto')
+
+        cardProduto.innerHTML = `
+            <h3>${nome}</h3>
+            <p><small>${caracteristicas}</small></p>
+            <p>Preço Base: R$ ${valorUnitario.toFixed(2)} / ${unidade}</p>
+            
+            <label>
+                Quantidade: 
+                <input type="number" class="input-qtd" value="1" min="1">
+            </label>
+            
+            <div class="resultados">
+                <p class="txt-total"></p>
+                <p class="txt-imposto"></p>
+                <p class="txt-final"></p>
+            </div>
+            
+            <button class="btn-remover">Remover Produto</button>
+            <hr>
+        `
+
+        const inputQuantidade = cardProduto.querySelector('.input-qtd')
+        const txtTotal = cardProduto.querySelector('.txt-total')
+        const txtImposto = cardProduto.querySelector('.txt-imposto')
+        const txtFinal = cardProduto.querySelector('.txt-final')
+        const btnRemover = cardProduto.querySelector('.btn-remover')
+
+        const atualizarValoresNaTela = () => {
+            const quantidadeAtual = Number(inputQuantidade.value)
+            const total = quantidadeAtual * valorUnitario
+            const valorDoImposto = total * calcularImposto(tipo)
+            const valorFinal = total + valorDoImposto
+
+            txtTotal.textContent = `Total Bruto: R$ ${total.toFixed(2)}`
+            txtImposto.textContent = `Imposto (Tipo ${tipo}): R$ ${valorDoImposto.toFixed(2)}`
+            txtFinal.textContent = `Valor Final: R$ ${valorFinal.toFixed(2)}`
+        }
+
+        atualizarValoresNaTela()
+        inputQuantidade.addEventListener('input', atualizarValoresNaTela)
+        btnRemover.addEventListener('click', () => removerProduto(id))
+
+        containerVitrine.appendChild(cardProduto)
+    })
+}
